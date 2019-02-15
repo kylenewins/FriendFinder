7c3d748f52bf0ddb550cf49269da2c73413ecea8
Toad Sage <kylenewins@gmail.com>
2019-02-15 18:52:41 -0500

    basic functionality

---
+++ app/data/friends.js
@@ -0,0 +1,85 @@
+var friends = [
+    {
+        name: "Ahmed",
+        photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores:[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      },
+      {
+        name: "Kyle",
+        photo:"https://media.licdn.com/dms/image/C5603AQEeVuC-6huSag/profile-displayphoto-shrink_200_200/0?e=1555545600&v=beta&t=xxvjun1wE_f0CYG_3Hj26Bh08VJtZkw8vy-eDPWjoO0",
+        scores:[
+            5,
+            3,
+            1,
+            4,
+            7,
+            4,
+            2,
+            3,
+            5,
+            1
+          ]
+      },
+      {
+        name: "Nic Cage",
+        photo:"http://s3.crackedcdn.com/blogimages/2009/03/cage5.jpg",
+        scores:[
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
+          ]
+      },
+      {
+        name: "Phillip J Fry",
+        photo:"https://oyster.ignimgs.com/mediawiki/apis.ign.com/futurama/1/11/Fry.jpg",
+        scores:[
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+          ]
+      },
+      {
+        name: "My Neighbor Bill",
+        photo:"https://thumbs.dreamstime.com/z/waving-senior-neighbor-outdoors-9087234.jpg",
+        scores:[
+            4,
+            3,
+            1,
+            3,
+            5,
+            2,
+            3,
+            3,
+            3,
+            3
+          ]
+      }
+
+  ];
+
+  module.exports = friends;
